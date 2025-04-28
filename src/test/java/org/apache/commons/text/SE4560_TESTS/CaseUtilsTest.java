78d58de270b46e6fb870ecb21a5eab0653be9f1a
Benjamin Petterborg <hello@ben-p.dev>
2025-04-27 22:47:17 -0400

    Add CaseUtils toCamelCase BVA tests.

---
+++ src/test/java/org/apache/commons/text/SE4560_TESTS/CaseUtilsTest.java
@@ -0,0 +1,31 @@
+package org.apache.commons.text.SE4560_TESTS;
+
+import org.apache.commons.text.CaseUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CaseUtilsTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "hellO/world, helloWorld",
+            "camel-case, camelCase",
+            "Number, number",
+            "Hello \uD83D\uDE00, hello\uD83D\uDE00",
+            "hi, hi"
+    })
+    void testToCamelCase(String input, String expected) {
+        char[] delimiters = {'/', '-'};
+        assertEquals(expected, CaseUtils.toCamelCase(input, false, delimiters));
+    }
+
+    // Other test for null input
+    @Test
+    void testToCamelCaseNullInput() {
+        assertNull(CaseUtils.toCamelCase(null, false));
+    }
+
+}
